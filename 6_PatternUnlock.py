0f9e67df92580e5531474dd3670738889009cbe1
zayakin <zayakin@sbermegamarket.ru>
2022-07-29 21:09:55 +0300

    6 quest

---
+++ 6_PatternUnlock.py
@@ -0,0 +1,20 @@
+
+def PatternUnlock(N: int, hits: list[int]) -> str:
+    katet = ['12', '19', '16', '21', '28', '23', '25', '32', '34', '37', '45', '43', '56', '52', '54', '61', '65', '73',
+             '78', '82', '87', '89', '91', '98']
+    gipotenyza = ['15', '18', '26', '29', '27', '24', '35', '38', '42', '51', '53', '62', '72', '83', '81', '92']
+    summ = 0.0
+
+    for index in range(N):
+        if index < N - 1:
+            step = f'{hits[index]}{hits[index+1]}'
+            if step in katet:
+                summ += 1.0
+            if step in gipotenyza:
+                summ += 1.41421
+    summ = str(summ)
+    result = ''
+    for i in summ:
+        if i in '123456789':
+            result += i
+    return result
