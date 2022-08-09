f0471fc7ee107faaf2e5ea3a54d7b860391647a5
zayakin <zayakin@sbermegamarket.ru>
2022-08-09 20:15:50 +0300

    12 quest

--- 12_MassVote.py
+++ 12_MassVote.py
@@ -4,12 +4,12 @@ from collections import Counter
 def MassVote(N: int, Votes: list[int]) -> str:
 
     percent_vote = sum(Votes)
-    max_vote = [0, 0]
+    max_vote = [0, 0, 0]
     counter_vote = Counter(Votes)
 
     for candidate, vote in enumerate(Votes):
         if vote > max_vote[1] and counter_vote[vote] == 1:
-            max_vote[0], max_vote[1] = candidate + 1, round((Votes[candidate] / percent_vote) * 100, 2)
+            max_vote[0], max_vote[1], max_vote[1] = candidate + 1, round((Votes[candidate] / percent_vote) * 100, 2), vote
 
     if max_vote[1] > 50:
         return f"majority winner {max_vote[0]}"
@@ -22,4 +22,4 @@ def MassVote(N: int, Votes: list[int]) -> str:
 example_list = [60, 10, 10, 15, 5]
 example_list_2 = [10, 10, 10, 45]
 
-print(MassVote(3, [23, 50, 27]))
+print(MassVote(2, [100, 99]))
