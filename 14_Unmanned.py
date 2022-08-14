56fae7482364cce9f64b4acfc70aedc3dbdb754a
zayakind <zayakind@gmail.com>
2022-08-14 15:35:21 +0300

    14 quest

--- 14_Unmanned.py
+++ 14_Unmanned.py
@@ -4,7 +4,8 @@ def created_track(lens: int, traffic_light: list[list[int]]) -> list[int, dict]:
     track = [1] * lens
     for lights in traffic_light:
         index = lights[0]
-        track[index] = {'red': lights[1], 'green': lights[2]}
+        if 0 < index <= lens:
+            track[index] = {'red': lights[1], 'green': lights[2]}
 
     return track
 
@@ -14,23 +15,25 @@ def Unmanned(L: int, N: int, track: list[list[int]]) -> int:
     track = created_track(L, track)
     time_to_path = 0
     section = 0
-
     while track[-1] != 0:
         section += 1
         road_section = track[section - 1]
-
         if not isinstance(road_section, dict):
             track[section - 1] = 0
             time_to_path += 1
+            print(track, time_to_path)
             continue
-
         if time_to_path - road_section['red'] < 0:
             track[section - 1] = 0
             time_to_path += road_section['red'] - time_to_path + 1
+            print(track, time_to_path)
             continue
-
         track[section - 1] = 0
         time_to_path += 1
+        print(track, time_to_path)
         continue
 
     return time_to_path
+
+
+print(Unmanned(10, 2, [[11, 5, 5], [15, 2, 2]]))
