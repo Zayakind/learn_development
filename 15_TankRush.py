88f068228abe0f685d353d7de4b6ffecbc3f41a3
zayakind <zayakind@gmail.com>
2022-08-19 07:03:28 +0300

    15 quest

---
+++ 15_TankRush.py
@@ -0,0 +1,32 @@
+
+def create_map(row: int, column: int, data: str) -> list[list[int]]:
+
+    result = [[] * column] * row
+    data = data.split(' ')
+
+    for index, row_iter in enumerate(result):
+        result[index] = list(int(x) for x in data[index])
+
+    return result
+
+
+def entry_map(card: list[list[int]], example: list[list[int]]) -> bool:
+
+    check = []
+    flag = False
+
+    for index_example, value_example in enumerate(example):
+        for index_card, value_card in enumerate(card):
+            if str(value_example).strip('[]') in str(value_card).strip('[]'):
+                check.append(value_example) if value_example not in check else None
+
+    if check == example:
+        flag = True
+
+    return flag
+
+
+def TankRush(line_1: int, column_1: int, data_1: str, line_2: int, column_2: int, data_2: str) -> bool:
+    first_map = create_map(line_1, column_1, data_1)
+    second_map = create_map(line_2, column_2, data_2)
+    return entry_map(first_map, second_map)
