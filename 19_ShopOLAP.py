0cde1ec17490a654a16a52dd57d2998909f15272
zayakind <zayakind@gmail.com>
2022-08-27 21:02:10 +0300

    19 quest

--- 19_ShopOLAP.py
+++ 19_ShopOLAP.py
@@ -1,7 +1,22 @@
 
-def ShopOLAP(N: int, items: list[str]) -> list[str]:
+def sort_list(data: dict) -> list[str]:
+
+    data = sorted(data.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+
+    for index, (key, value) in enumerate(data):
+        if index > 0 and data[index - 1][0] > key and data[index - 1][1] == value:
+            data.insert(index, data.pop(index - 1))
 
     result = []
+
+    for item in data:
+        result.append(f"{item[0]} {item[1]}")
+
+    return result
+
+
+def ShopOLAP(N: int, items: list[str]) -> list[str]:
+
     temp_dict = {}
 
     for item in items:
@@ -11,12 +26,6 @@ def ShopOLAP(N: int, items: list[str]) -> list[str]:
             continue
         temp_dict[itm] = int(sale)
 
-    temp_dict = {key: value for key, value in sorted(temp_dict.items(), key=lambda item: item[1], reverse=True)}
-
-    for key, value in temp_dict.items():
-        result.append(f'{key} {value}')
-
-    result.sort()
+    result = sort_list(temp_dict)
 
     return result
-
