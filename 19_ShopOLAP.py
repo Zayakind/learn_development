2f71819312305e029a19d832498eb0d5c42e2777
zayakind <zayakind@gmail.com>
2022-08-28 11:28:11 +0300

    19 quest

--- 19_ShopOLAP.py
+++ 19_ShopOLAP.py
@@ -3,9 +3,15 @@ def sort_list(data: dict) -> list[str]:
 
     data = sorted(data.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
 
-    for index, (key, value) in enumerate(data):
-        if index > 0 and data[index - 1][0] > key and data[index - 1][1] == value:
-            data.insert(index, data.pop(index - 1))
+    while True:
+        flag = False
+        for index, (key, value) in enumerate(data):
+            if index > 0 and data[index - 1][0] > key and data[index - 1][1] == value:
+                data.insert(index, data.pop(index - 1))
+                flag = True
+
+        if not flag:
+            break
 
     result = []
 
@@ -29,3 +35,6 @@ def ShopOLAP(N: int, items: list[str]) -> list[str]:
     result = sort_list(temp_dict)
 
     return result
+
+
+print(ShopOLAP(8,['123 5','32 3','124 5','128 1','32 2','23 4','128 4','128 1']))
