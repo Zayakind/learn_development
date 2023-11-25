fc3a5746432a6d137b11b85011df3f566122c20c
zayakin <zayakin@sbermegamarket.ru>
2023-11-25 20:19:33 +0300

    Update func

--- 21_BiggerGreater.py
+++ 21_BiggerGreater.py
@@ -3,7 +3,7 @@ from itertools import *
 
 def BiggerGreater(string: str) -> str:
     sort_options = sorted(set(map(''.join, permutations(string))))
-    if len(sort_options) <= 1 and sort_options[-1] == string:
+    if len(sort_options) <= 1 or sort_options[-1] == string:
         return ""
     index = sort_options.index(string)
     return sort_options[index+1]
