9ed89a3faf3da91671c3f10c202c82270f3c234e
zayakin <zayakin@sbermegamarket.ru>
2023-11-26 13:55:16 +0300

    New func

---
+++ survivor/test_walkers.py
@@ -0,0 +1,21 @@
+from white_walkers_26 import white_walkers
+
+
+def test_1():
+    assert white_walkers("axxb6===4xaf5===eee5")
+
+
+def test_2():
+    assert not white_walkers("5==ooooooo=5=5")
+
+
+def test_3():
+    assert white_walkers("abc=7==hdjs=3gg1=======5")
+
+
+def test_4():
+    assert not white_walkers("aaS=8")
+
+
+def test_5():
+    assert white_walkers("9===1===9===1===9")
---
+++ survivor/white_walkers_26.py
@@ -0,0 +1,26 @@
+def search_digit_indexes(value: str) -> list:
+    return [i for i in range(len(value)) if value[i].isdigit()]
+
+
+def count_white_walkers(substring: str) -> int:
+    return substring.count('=')
+
+
+def white_walkers(village: str) -> bool:
+    digit_indexes = search_digit_indexes(village)
+
+    if len(digit_indexes) <= 1:
+        return False
+
+    count_pairs, count_walkers = 0, 0
+    for i in range(len(digit_indexes) - 1):
+        if (int(village[digit_indexes[i]]) +
+                int(village[digit_indexes[i + 1]]) != 10):
+            continue
+        count_pairs += 1
+        amount_white_walkers = count_white_walkers(
+            village[digit_indexes[i]:digit_indexes[i + 1] + 1]
+        )
+        if amount_white_walkers == 3:
+            count_walkers += 1
+    return count_walkers == count_pairs
