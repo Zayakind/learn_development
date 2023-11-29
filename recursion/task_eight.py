2b6c1bf9725992b45dd42972d57ce9adb9420845
zayakin <zayakin@sbermegamarket.ru>
2023-11-29 21:05:16 +0300

    New func

--- recursion/task_eight.py
+++ recursion/task_eight.py
@@ -3,19 +3,16 @@ import os
 
 def search_files(directory, files: list):
 
-    # name_dir = os.listdir(directory)
-    #
-    # [files.append(name) if os.path.isfile(name) and "." in name else search_files(name, files) for name in name_dir]
-
-    for dir_path, dir_names, filenames in os.walk(directory):
-        [files.append(file) for file in filenames]
-
-        for dir in dir_names:
-            search_files(os.path.join(dir_path, dir), files)
+    entries = os.listdir(directory)
+    for entry in entries:
+        full_path = os.path.join(directory, entry)
+        if os.path.isfile(full_path):
+            files.append(full_path)
+        elif os.path.isdir(full_path):
+            search_files(full_path, files)
 
 
 def find_files(directory):
     files = []
-
     search_files(directory, files)
     return files
