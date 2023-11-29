f32b47c6c1837ec8b948fae720641f291f268627
zayakin <zayakin@sbermegamarket.ru>
2023-11-29 17:09:56 +0300

    New func

--- recursion/task_seven.py
+++ recursion/task_seven.py
@@ -1,16 +1,16 @@
 
-def tail_recursive(numbers: list, first_max: float | int, second_max: float | int) -> int:
+def search_recursive(numbers: list, first_max: float | int, second_max: float | int) -> int:
     if not numbers:
         return second_max
     temp = numbers.pop(0)
     if temp >= first_max:
-        return tail_recursive(numbers, temp, first_max)
+        return search_recursive(numbers, temp, first_max)
     if temp > second_max:
-        return tail_recursive(numbers, first_max, temp)
-    return tail_recursive(numbers, first_max, second_max)
+        return search_recursive(numbers, first_max, temp)
+    return search_recursive(numbers, first_max, second_max)
 
 
 def find_second_max(data: list) -> int:
     first_max = float('-inf')
     second_max = float('-inf')
-    return tail_recursive(data, first_max, second_max)
+    return search_recursive(data, first_max, second_max)
