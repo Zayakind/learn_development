b28c1a8cf74b2d5a31c874225d7db58132404c34
zayakin <zayakin@sbermegamarket.ru>
2023-12-05 18:41:58 +0300

    New func

--- eight_algoritms/eec_help.py
+++ eight_algoritms/eec_help.py
@@ -1,7 +1,14 @@
 def EEC_help(array_one: list[int], array_two: list[int]) -> bool:
     if len(array_one) != len(array_two):
         return False
-    for one, two in zip(sorted(array_one), sorted(array_two)):
-        if one != two:
+
+    counts = {}
+    for num in array_one:
+        counts[num] = counts.get(num, 0) + 1
+
+    for num in array_two:
+        if num not in counts or counts[num] == 0:
             return False
+        counts[num] -= 1
+
     return True
--- tests/test_eec_help.py
+++ tests/test_eec_help.py
@@ -1,20 +1,20 @@
 from eight_algoritms.eec_help import EEC_help
 
 def test_1():
-    assert EEC_help([1,2,3], [1,2,3,4]) == False
+    assert not EEC_help([1,2,3], [1,2,3,4])
 
 
 def test_2():
-    assert EEC_help([1,2,3], [1,2,3]) == True
+    assert EEC_help([1,2,3], [1,2,3])
 
 
 def test_3():
-    assert EEC_help([1,3,2], [1,2,3]) == True
+    assert EEC_help([1,3,2], [1,2,3])
 
 
 def test_4():
-    assert EEC_help([1,3,2,3], [1,2,2,3]) == False
+    assert not EEC_help([1,3,2,3], [1,2,2,3])
 
 
 def test_5():
-    assert EEC_help([1,1], [1,1]) == True
+    assert EEC_help([1,1], [1,1])
