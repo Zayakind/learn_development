c28596a41abb1f2f29c51e29eda243a9c9b43683
zayakin <zayakin@sbermegamarket.ru>
2023-12-08 16:14:38 +0300

    New func

---
+++ tests/test_matrix.py
@@ -0,0 +1,11 @@
+from eight_algoritms.matrix import matrix
+
+
+def test_matrix():
+    mtrx = [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+    ]
+    assert matrix(5, 4, mtrx) == [1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12]
