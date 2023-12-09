eb3c41ca9dd219463e2177a83204e97ef7c6d348
zayakin <zayakin@sbermegamarket.ru>
2023-12-10 01:10:06 +0300

    New func

--- eight_algoritms/army_communication_matrix.py
+++ eight_algoritms/army_communication_matrix.py
@@ -1,7 +1,6 @@
 def army_communication_matrix(n: int, matrix: list) -> str:
     dp = [[0] * (n + 1) for _ in range(n + 1)]
 
-    # заполнение матрицы dp
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             dp[i][j] = matrix[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
@@ -10,7 +9,6 @@ def army_communication_matrix(n: int, matrix: list) -> str:
     max_coords = (0, 0)
     max_size = 0
 
-    # поиск максимальной суммы и сохранение координат и размера подматрицы
     for m in range(2, n):
         for i in range(n - m + 1):
             for j in range(n - m + 1):
